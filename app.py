2de3c34cc8336b4accc67464785cc5633f1159a7
Hina Ashar <hinaashar2020@gmail.com>
2026-03-25 17:29:08 +0500

    Add Initial Files

---
+++ app.py
@@ -0,0 +1,6 @@
+# simple app that greets the user by name
+user = input("Enter your name: ")
+print(f"Hello, {user}! Welcome to the app.")
+# This is a simple app that greets the user by name. You can expand this app by adding more features or functionalities as needed.
+
+
