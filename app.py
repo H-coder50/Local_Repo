21d9683c36a3292905cac8ba12a44903bd74be08
Hina Ashar <hinaashar2020@gmail.com>
2026-03-25 19:22:04 +0500

    updated app.py code

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ print(f"Hello, {user}! Welcome to the app.")
 # This is a simple app that greets the user by name. You can expand this app by adding more features or functionalities as needed.
 
 
+user_input = input("Would you like to see a joke? (yes/no): ")
+if user_input.lower() == "yes":
+    print("Why don't scientists trust atoms? Because they make up everything!")
+else:    print("No worries! Have a great day!")
